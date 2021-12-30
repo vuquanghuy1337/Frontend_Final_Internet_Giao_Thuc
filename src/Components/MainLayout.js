e921fb7475f222dcf47fe812dcee0b5d116a65c1
Huy <vuquanghuy1337@gmail.com>
2021-12-30 19:03:29 +0700

    t sua file

--- src/Components/MainLayout.js
+++ src/Components/MainLayout.js
@@ -5,6 +5,7 @@ import {
   UserOutlined,
   FileOutlined,
   PieChartOutlined,
+  BellOutlined
 } from "@ant-design/icons";
 import { Col, Row } from "antd";
 import { useNavigate } from "react-router-dom";
@@ -12,7 +13,7 @@ import { useNavigate } from "react-router-dom";
 function RouteCard(props) {
   let navigate = useNavigate();
 
-  return (
+  return (<>
     <div
       className="RouteCard rounded p-1"
       onClick={() => {
@@ -20,15 +21,18 @@ function RouteCard(props) {
       }}
     >
       <div
-        className="d-flex p-2 rounded-top"
+        className="d-flex p-2 rounded-top "
         style={{ background: props.background }}
       >
-        <div className="d-flex flex-column">
+        <div className="d-flex flex-column left" >
           <span style={{ "font-size": "1.5rem" }}>{props.title}</span>
           <span style={{ "font-size": "1rem" }}>{props.des}</span>
         </div>
-        <div className="mx-2">{props.icon}</div>
+        <div className="mx-2 right">{props.icon}</div>
+
+
       </div>
+
       <div
         className="d-flex justify-content-center align-items-center rounded-bottom"
         style={{ background: props.background2 }}
@@ -36,21 +40,29 @@ function RouteCard(props) {
         <span className="mx-1">Xem thêm</span> <RightCircleOutlined />
       </div>
     </div>
+    </>
   );
 }
 
 export default function MainLayout() {
   return (
-    <div>
+    <div className="cw">
+
       <Row>
-        <RouteCard
-          title="Theo dõi công việc"
-          des="Việc cần làm, deadline..."
-          background={"hsl(0, 100%, 50%)"}
+        <Col span={8}>
+          {/* <div className="cw"> */}
+        <RouteCard 
+        title="Công việc"
+        des="Việc cần làm, deadline..."
+          background={"hsl(0, 80%, 50%)"}
           background2={"hsl(0, 50%, 50%)"}
+          icon={<CalendarOutlined style={{ fontSize: "5em" }}/> }
           url="todo"
-          icon={<CalendarOutlined style={{ fontSize: "5em" }} />}
-        />
+          />
+        
+
+        </Col>
+        <Col span={8}>
         <RouteCard
           title="Trao đổi"
           des="Đọc gửi thư điện tử"
@@ -59,16 +71,21 @@ export default function MainLayout() {
           url="mail"
           icon={<CommentOutlined style={{ fontSize: "5em" }} />}
         />
+        </Col>
+        <Col span={8}>
         <RouteCard
           title="Bảng tin"
           des="Các hoạt động của lớp học"
           background={"hsl(40, 100%, 50%)"}
           background2={"hsl(40, 50%, 50%)"}
           url="blog"
-          icon={<CalendarOutlined style={{ fontSize: "5em" }} />}
+          icon={<BellOutlined style={{ fontSize: "5em" }} />}
         />
+        </Col>
       </Row>
+
       <Row>
+        <Col span={8}>
         <RouteCard
           title="Bảng điểm"
           des="Điểm số của từng sinh viên"
@@ -77,14 +94,18 @@ export default function MainLayout() {
           url="score"
           icon={<PieChartOutlined style={{ fontSize: "5em" }} />}
         />
+        </Col>
+        <Col span={8}>
         <RouteCard
-          title="Thông tin sinh viên"
+          title="Sinh viên"
           des="Thông tin cá nhân của từng sinh viên..."
           background={"hsl(180, 100%, 50%)"}
           background2={"hsl(180, 50%, 50%)"}
           url="student"
           icon={<UserOutlined style={{ fontSize: "5em" }} />}
         />
+        </Col>
+        <Col span={8}>
         <RouteCard
           title="Thư viện"
           des="Bài giảng, tạp chí..."
@@ -93,6 +114,7 @@ export default function MainLayout() {
           url="library"
           icon={<FileOutlined style={{ fontSize: "5em" }} />}
         />
+        </Col>
       </Row>
     </div>
   );
